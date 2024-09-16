07a6ed26303c21c06d2598dd9f630a1312783a35
MasuqaT (T.Okada) <occar@hotmail.co.jp>
2024-09-16 12:43:13 +0900

    Move env var setting

--- devtools-script.ps1
+++ devtools-script.ps1
@@ -8,39 +8,42 @@ scoop install git
 scoop bucket add extras
 scoop install sudo
 
-# Dev Drive
-# Assuming X is Dev Drive
-mkdir -p X:\packages\npm
-setx npm_config_cache X:\packages\npm
-mkdir -p X:\packages\pip
-setx PIP_CACHE_DIR X:\packages\pip
-mkdir -p X:\packages\cargo
-setx CARGO_HOME X:\packages\cargo
-mkdir -p X:\packages\go
-setx GOCACHE X:\packages\go
-mkdir -p X:\packages\yarn
-setx YARN_CACHE_FOLDER X:\packages\yarn
-mkdir -p X:\packages\deno
-setx DENO_DIR X:\packages\deno
-
 ####################
 # Version Managers #
 ####################
 scoop install nvm # Node(JavaScript)
 scoop install pyenv
+
 scoop install rustup
+mkdir -p X:\packages\cargo
+setx CARGO_HOME X:\packages\cargo
+
 scoop install go
+mkdir -p X:\packages\go
+setx GOCACHE X:\packages\go
 
 #########
 # Tools #
 #########
 nvm install lts
 nvm use lts
+mkdir -p X:\packages\npm
+setx npm_config_cache X:\packages\npm
+
 scoop install yarn
+mkdir -p X:\packages\yarn
+setx YARN_CACHE_FOLDER X:\packages\yarn
+
 scoop install deno
+mkdir -p X:\packages\deno
+setx DENO_DIR X:\packages\deno
+
 scoop install jetbrains-toolbox
 scoop install gow
+
 python -m ensurepip --upgrade
+mkdir -p X:\packages\pip
+setx PIP_CACHE_DIR X:\packages\pip
 
 ###########
 # Visuals #
