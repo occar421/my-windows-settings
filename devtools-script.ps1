679c65d679f23a5e5bfbb1b75b9f845e27c5d0f3
MasuqaT (T.Okada) <occar@hotmail.co.jp>
2026-03-20 17:44:11 +0900

    Update devtools-script.ps1

--- devtools-script.ps1
+++ devtools-script.ps1
@@ -6,11 +6,12 @@ iwr -useb get.scoop.sh | iex
 # Basics
 scoop install git
 scoop bucket add extras
-scoop install sudo
+scoop install gow  # GNU on Windows
 scoop install mise
 
 # Programming Languages
 mise ls-remote node
+mise ls-remote deno
 mise ls-remote python
 mise ls-remote rust
 mise ls-remote go
